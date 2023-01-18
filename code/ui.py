ae7da9491bf6e61d3e15b8abfadda897561e99b5
Maciej Szubiczuk <maciej.szubiczuk1@gmail.com>
2023-01-18 22:56:39 +0100

    changed the positon of the health bar

--- code/ui.py
+++ code/ui.py
@@ -8,16 +8,22 @@ class UI:
         self.font = pygame.font.Font(UI_FONT,UI_FONT_SIZE)
 
         # bar setup 
-        self.health_bar_rect = pygame.Rect(60,600,HEALTH_BAR_WIDTH,BAR_HEIGHT)
-        self.energy_bar_rect = pygame.Rect(60,650,ENERGY_BAR_WIDTH,BAR_HEIGHT)
+        self.health_bar_rect = pygame.Rect(60,630,HEALTH_BAR_WIDTH,BAR_HEIGHT)
+        #self.energy_bar_rect = pygame.Rect(60,650,ENERGY_BAR_WIDTH,BAR_HEIGHT)
 
     def show_bar(self,current,max_amount,bg_rect,color):
         pygame.draw.rect(self.display_surface,UI_BG_COLOR,bg_rect)
 
         # converting int stats to pixel to display in a bar
-        
+        ratio = current / max_amount
+        current_width = bg_rect.width * ratio
+        current_rect = bg_rect.copy()
+        current_rect.width = current_width
+
+        # drawing the actual bar
+        pygame.draw.rect(self.display_surface,color,current_rect)
 
 
     def display(self,player):
         self.show_bar(player.health,player.stats['health'],self.health_bar_rect,HEALTH_COLOR)
-        self.show_bar(player.energy,player.stats['energy'],self.energy_bar_rect,ENERGY_COLOR)
+        #self.show_bar(player.energy,player.stats['energy'],self.energy_bar_rect,ENERGY_COLOR)
