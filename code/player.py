b6abb464cfa78f955f7fadfac137cd9361a37577
Maciej Szubiczuk <maciej.szubiczuk1@gmail.com>
2023-01-18 21:55:49 +0100

    added sword sprite that players attacks with

--- code/player.py
+++ code/player.py
@@ -3,7 +3,7 @@ from settings import *
 from support import import_folder
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self,pos,groups,obstacle_sprites):
+    def __init__(self,pos,groups,obstacle_sprites,create_attack,destroy_attack):
 
         super().__init__(groups)
         self.image = pygame.image.load('graphics/testAssets/player.png').convert_alpha()
@@ -22,8 +22,13 @@ class Player(pygame.sprite.Sprite):
         self.attacking = False
         self.attack_cooldown = 400
         self.attack_time = None
-
         self.obstacle_sprites = obstacle_sprites
+
+        # weapon
+        self.create_attack = create_attack
+        self.destroy_attack = destroy_attack
+        self.weapon_index = 0
+        self.weapon = list(weapon_data.keys())[self.weapon_index]
 
     def import_player_assets(self):
         character_path = 'graphics/player/'
@@ -79,7 +84,7 @@ class Player(pygame.sprite.Sprite):
             if keys[pygame.K_SPACE] and not self.attacking:
                 self.attacking=True
                 self.attack_time = pygame.time.get_ticks()
-                print('attack')
+                self.create_attack()
 
             # input for magic
             if keys[pygame.K_LCTRL] and not self.attacking:
@@ -122,6 +127,7 @@ class Player(pygame.sprite.Sprite):
         if self.attacking:
             if current_time - self.attack_time >= self.attack_cooldown:
                 self.attacking = False
+                self.destroy_attack()
 
     def animate(self):
         animation = self.animations[self.status]
